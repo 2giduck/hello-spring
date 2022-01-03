b54fcce9c14c74260ee583170fe2b0ca74047281
Dulgiduck <jsh@sngy.io>
2022-01-03 17:52:36 +0900

    220103 - View 환경설정

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -10,6 +10,9 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
-        return "hello";
+        return "hello"; // hello.html로 연결
+        // 문자를 반환하면 뷰 리졸버(viewResolver)가 화면을 찾아서 처리
+        // resources:templates/ + {viewName} + .html
+        // spring-boot-devtools 라이브러리 추가하면 재시작 없이 바꾼 html바로 볼 수 있음
     }
 }
